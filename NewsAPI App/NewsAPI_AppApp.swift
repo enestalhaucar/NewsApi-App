2ba079ae60d084d9b7791febb09dc6b4199fb478
Enes Talha Uçar <talhauenes@gmail.com>
2024-05-12 19:35:25 +0300

    Created NewsApi App

---
+++ NewsAPI App/NewsAPI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPI_AppApp.swift
+//  NewsAPI App
+//
+//  Created by Enes Talha Uçar  on 9.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
